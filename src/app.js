a094494412d07d304fb04c49887b045b6bc01ac0
nathanremond <perso.nathan.remond@gmail.com>
2025-11-13 12:10:08 +0100

    feat: Add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 const userRoutes = require("./routes/userRoutes.js");
 app.use("/api/users", userRoutes);
