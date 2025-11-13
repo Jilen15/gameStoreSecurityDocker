745b7754672d639c8f3a23ba9f8e45ba2a1eb7d9
nathanremond <perso.nathan.remond@gmail.com>
2025-11-13 12:27:06 +0100

    feat: Add ratelimit with a limit of 10 request per minutes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+    rateLimit({
+        windowMs: 60*1000,
+        max: 10,
+        message: {
+            status: 429,
+            message: "Trop de requêtes. Réessayez dans une minute."
+        },
+        standardHeaders: true,
+        legacyHeaders: false
+    })
+);
 
 const userRoutes = require("./routes/userRoutes.js");
 app.use("/api/users", userRoutes);
