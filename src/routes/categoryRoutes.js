ec28faa903f18172ff23147a7394f9cec82e2103
nathanremond <perso.nathan.remond@gmail.com>
2025-11-13 16:01:04 +0100

    fix: Update route and controller imports to ES6 syntax and add authentication middleware

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import CategoryController from "../controllers/categoryController.js";
+import { authenticateToken } from "../middlewares/Auth.js";
+import { authorizeRoles } from "../middlewares/Roles.js";
 
 const router = express.Router();
 
-router.get("/", CategoryController.listCategories);
-router.get("/:id", CategoryController.getCategory);
-router.post("/", CategoryController.createCategory);
-router.put("/:id", CategoryController.updateCategory);
-router.delete("/:id", CategoryController.deleteCategory);
+router.get("/", authenticateToken, CategoryController.listCategories);
+router.get("/:id", authenticateToken, CategoryController.getCategory);
+router.post("/", authenticateToken, authorizeRoles("admin"), CategoryController.createCategory);
+router.put("/:id", authenticateToken, authorizeRoles("admin"), CategoryController.updateCategory);
+router.delete("/:id", authenticateToken, authorizeRoles("admin"), CategoryController.deleteCategory);
 
 export default router;
--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import GameController from "../controllers/gameController.js";
+import { authenticateToken } from "../middlewares/Auth.js";
+import { authorizeRoles } from "../middlewares/Roles.js";
 
 const router = express.Router();
 
-router.get("/", GameController.listGames);
-router.get("/:id", GameController.getGame);
-router.post("/", GameController.createGame);
-router.put("/:id", GameController.updateGame);
-router.delete("/:id", GameController.deleteGame);
+router.get("/", authenticateToken, GameController.listGames);
+router.get("/:id", authenticateToken, GameController.getGame);
+router.post("/", authenticateToken, authorizeRoles("admin"), GameController.createGame);
+router.put("/:id", authenticateToken, authorizeRoles("admin"), GameController.updateGame);
+router.delete("/:id", authenticateToken, authorizeRoles("admin"), GameController.deleteGame);
 
 export default router;
